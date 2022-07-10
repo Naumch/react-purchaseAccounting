99e0624558839bc3675217769242c857e50b150b
Anna <anya.naumova.1994@gmail.com>
2022-07-10 12:18:24 +0300

    Удалила лишний компонент FakeTable

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,36 +1,36 @@
-import styled from 'styled-components';
 import TableFilter from './TableFilter';
 import Rows from './Rows';
 import Total from './Total';
-import FakeTable from './FakeTable';
-
-const Display = styled.div`
-	display: ${props => props.filteredProducts.length === 0 ? 'none' : 'block'};
-`;
 
 function Table({ products, setProducts, categories, filter, setFilter, unix, setUnix, filteredProducts }) {
 	
-  return <div className='table-wrapper'>
-		<TableFilter 
-			filter={filter}
-			setFilter={setFilter}
-			categories={categories}
-			unix={unix}
-			setUnix={setUnix}
-			products={products}
-		/>
-		<FakeTable filteredProducts={filteredProducts}/>
-		<Display filteredProducts={filteredProducts}>
-			<table>
-				<Rows 
-					products={products}
-					setProducts={setProducts}
-					filteredProducts={filteredProducts}
-				/>
-			</table>
-			<Total filteredProducts={filteredProducts}/>
-		</Display>
-	</div>;
+  return (
+		<div className='table-wrapper'>
+			<TableFilter 
+				filter={filter}
+				setFilter={setFilter}
+				categories={categories}
+				unix={unix}
+				setUnix={setUnix}
+				products={products}
+			/>
+			{filteredProducts.length === 0 
+				? <div>
+						<p>Покупок пока не было...</p>
+					</div>
+				: <div>
+						<table>
+							<Rows 
+								products={products}
+								setProducts={setProducts}
+								filteredProducts={filteredProducts}
+							/>
+						</table>
+						<Total filteredProducts={filteredProducts}/>
+					</div>
+			}
+		</div>
+	)
 }
 
 export default Table;
